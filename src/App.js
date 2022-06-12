720a27f337ba5f049e5f6537ceabd4a8d628ede7
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-06-12 14:45:46 +0300

    added component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./style.scss"
 import React from "react";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 class App extends React.Component {
   render(){
     return(
       <div className="App">
-        <h1>hello</h1>
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
       </div>
     );
   }
