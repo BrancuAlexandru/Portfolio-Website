97d5a576316bb0d1dd04369ca7e59a3d466abfb7
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-07-31 12:03:50 +0300

    added credits button that opens into a larger div with links, names and what I used

---
+++ src/components/Credits.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Credits = () => {
+  const [creditsIsOpen, setCreditsIsOpen] = useState(false);
+  const creditsList = [
+    ['BGJar', 'https://bgjar.com', '- For the background'],
+    ['SVG Repo', 'https://www.svgrepo.com', '- For icons'],
+    ['Icons8', 'https://icons8.com', '- For icons']
+  ]// if credits === open then render open if not render closed; this should be a stateful var
+  return (
+    <div className="Credits">
+      {creditsIsOpen && 
+        <div className="open">
+          <div className="credits-left-side" onClick={() => setCreditsIsOpen(false)}></div>
+          <div className="credits-wrapper">
+            <h2>Special thanks to:</h2>
+            {creditsList.map((item, id) => {
+              return (
+                <div className="credit-element" key={id}>
+                  <h3>{item[0]}</h3>
+                  <a target="_blank" href={item[1]}>{item[1]}</a>
+                  <p>{item[2]}</p>
+                </div>
+              )
+            })}
+          </div>
+          <div className="credits-right-side" onClick={() => setCreditsIsOpen(false)}></div>
+        </div>
+      }
+      {!creditsIsOpen && 
+        <div className="closed">
+          <a className="credits-button cta" onClick={() => setCreditsIsOpen(true)}>Credits</a>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default Credits;
