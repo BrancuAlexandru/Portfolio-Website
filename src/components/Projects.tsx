d1b2aa6c1196eb97e488a782c4aaee548f173bbe
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-06-24 15:40:28 +0300

    Added 'expandable' icon to projects for clear communication that the images can be clicked

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,6 +27,11 @@ const Projects:FC = () => {
             }}>
             <div className="project-image">
               <img src="./images/authentication.webp" alt="screenshot of the log in for the project" loading="lazy"/>
+              <div id="expandIcon">
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8.5V4M4 4H8.5M4 4L9.5 9.5M20 8.5V4M20 4H15.5M20 4L14.5 9.5M4 15.5V20M4 20H8.5M4 20L9.5 14.5M20 15.5V20M20 20H15.5M20 20L14.5 14.5"/>
+                </svg>
+              </div>
             </div>
           </div>
           <div className="project cta" onClick={() => {
@@ -36,6 +41,11 @@ const Projects:FC = () => {
             }}>
             <div className="project-image">
               <img src="./images/landingPage.webp" alt="screenshot of the portfolio" loading="lazy"/>
+              <div id="expandIcon">
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8.5V4M4 4H8.5M4 4L9.5 9.5M20 8.5V4M20 4H15.5M20 4L14.5 9.5M4 15.5V20M4 20H8.5M4 20L9.5 14.5M20 15.5V20M20 20H15.5M20 20L14.5 14.5"/>
+                </svg>
+              </div>
             </div>
           </div>
           <div className="project cta" onClick={() => {
@@ -45,6 +55,11 @@ const Projects:FC = () => {
             }}>
             <div className="project-image">
               <img src="./images/gamefinderScreenshot.webp" alt="screenshot of Gamefinder extension" loading="lazy"/>
+              <div id="expandIcon">
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8.5V4M4 4H8.5M4 4L9.5 9.5M20 8.5V4M20 4H15.5M20 4L14.5 9.5M4 15.5V20M4 20H8.5M4 20L9.5 14.5M20 15.5V20M20 20H15.5M20 20L14.5 14.5"/>
+                </svg>
+              </div>
             </div>
           </div>
           <div className="blank-project-left"></div>
